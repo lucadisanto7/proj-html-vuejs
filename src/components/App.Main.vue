0aca5915c46ed77c32db14fd2e42984b12cc730a
lucadisanto7 <lucadisanto47@gmail.com>
2024-09-01 15:37:30 +0200

    [UPD] Added text

--- src/components/App.Main.vue
+++ src/components/App.Main.vue
@@ -9,7 +9,17 @@ export default {
         <img src="../imgs/middle.png" alt="">
     </div>
     <main>
-        
+        <div class="container mt-350">
+            <div class="row">
+                <div class="col-12">
+                    <div class="content text-center">
+                        <h3>Our Love for animation</h3>
+                        <h2 class="fs-50">We're Storytelling <br> Experts With Creative <br> Smarts</h2>
+                        <h5 class="mt-4">our culture inspires the creative and innovative spirit found troughout <br> our Studio. We breath life into new ideas!</h5>
+                    </div>    
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 <style lang="scss">
@@ -25,5 +35,11 @@ export default {
         position: absolute;
         top: 50%;
         left: 25%;
+    }
+    .mt-350{
+        margin-top: 350px;
+    }
+    .fs-50{
+        font-size: 60px;
     }
 </style>
