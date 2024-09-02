885da15d47a35228804f27f3a0e357f65bbc9306
lucadisanto7 <lucadisanto47@gmail.com>
2024-09-02 15:22:52 +0200

    [UPD] Added footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,13 +1,57 @@
-<template lang="">
-    <div>
-        
-    </div>
-</template>
 <script>
 export default {
     
 }
 </script>
-<style lang="">
+<template>
+    <footer>
+        <div class="container">
+            <div class="row mt-150">
+                <div class="col-6">
+                    <div class="content mb-3">
+                        <img src="../imgs/footer-logo.png" alt="logo">
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="content">
+                        <ul class="list">
+                            <li><a href="" class="list-itm black-text">About us</a></li>
+                            <li><a href="" class="list-itm black-text">Portfolio</a></li>
+                            <li><a href="" class="list-itm black-text">Pricing</a></li>
+                            <li><a href="" class="list-itm black-text">Career</a></li>
+                            <li><a href="" class="list-itm black-text">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <hr>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <div class="content mt-2">
+                        <div>
+                            <p>Copyright 2022 &copy; 2022 <span class="violet-text">Softivus.</span> All rights reserved</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="content mt-2">
+                        <ul class="list">
+                            <li><a href="" class="list-itm black-text"><svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-facebook social" viewBox="0 0 16 16">
+                                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+                            </svg></a></li>
+                            <li><a href="" class="list-itm black-text"><svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-twitter social" viewBox="0 0 16 16">
+                                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334q.002-.211-.006-.422A6.7 6.7 0 0 0 16 3.542a6.7 6.7 0 0 1-1.889.518 3.3 3.3 0 0 0 1.447-1.817 6.5 6.5 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.32 9.32 0 0 1-6.767-3.429 3.29 3.29 0 0 0 1.018 4.382A3.3 3.3 0 0 1 .64 6.575v.045a3.29 3.29 0 0 0 2.632 3.218 3.2 3.2 0 0 1-.865.115 3 3 0 0 1-.614-.057 3.28 3.28 0 0 0 3.067 2.277A6.6 6.6 0 0 1 .78 13.58a6 6 0 0 1-.78-.045A9.34 9.34 0 0 0 5.026 15"/>
+                            </svg></a></li>
+                            <li><a href="" class="list-itm black-text"> <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-threads social" viewBox="0 0 16 16">
+                                <path d="M6.321 6.016c-.27-.18-1.166-.802-1.166-.802.756-1.081 1.753-1.502 3.132-1.502.975 0 1.803.327 2.394.948s.928 1.509 1.005 2.644q.492.207.905.484c1.109.745 1.719 1.86 1.719 3.137 0 2.716-2.226 5.075-6.256 5.075C4.594 16 1 13.987 1 7.994 1 2.034 4.482 0 8.044 0 9.69 0 13.55.243 15 5.036l-1.36.353C12.516 1.974 10.163 1.43 8.006 1.43c-3.565 0-5.582 2.171-5.582 6.79 0 4.143 2.254 6.343 5.63 6.343 2.777 0 4.847-1.443 4.847-3.556 0-1.438-1.208-2.127-1.27-2.127-.236 1.234-.868 3.31-3.644 3.31-1.618 0-3.013-1.118-3.013-2.582 0-2.09 1.984-2.847 3.55-2.847.586 0 1.294.04 1.663.114 0-.637-.54-1.728-1.9-1.728-1.25 0-1.566.405-1.967.868ZM8.716 8.19c-2.04 0-2.304.87-2.304 1.416 0 .878 1.043 1.168 1.6 1.168 1.02 0 2.067-.282 2.232-2.423a6.2 6.2 0 0 0-1.528-.161"/>
+                              </svg></a></li>
+                        </ul>
+                    </div>                   
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+<style lang="scss">
     
 </style>
